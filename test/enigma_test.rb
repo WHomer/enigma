03616197cb29926c3320b1221519b797ca769e93
William Homer <billyhomeriii@gmail.com>
2019-04-11 14:51:53 -0600

    Add test to make sure Enigma is getting instanciated

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -2,6 +2,12 @@ require 'minitest/autorun'
 require './lib/enigma'
 
 class EnigmaTest < MiniTest::Test
+  def setup
+    @enigma_1 = Enigma.new()
+  end
 
+  def test_it_exists
+    assert_instance_of Enigma, @enigma_1
+  end
 
 end
