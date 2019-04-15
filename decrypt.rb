fd923210c050e95ec42a300312c27ac6c502ba2d
William Homer <billyhomeriii@gmail.com>
2019-04-15 15:35:54 -0600

    Add logic to decrypt a file through cli

---
+++ decrypt.rb
@@ -0,0 +1,11 @@
+require './lib/enigma'
+
+enigma = Enigma.new
+encrypted_file = ARGV[0]
+decrypted_file = ARGV[1]
+key = ARGV[2]
+date = ARGV[3]
+message = File.read("./encrypted/#{encrypted_file}").chomp
+results = enigma.decrypt(message, key, date)
+File.write("./encrypted/#{decrypted_file}", results[:decryption])
+puts "Created \'#{decrypted_file}\' with the key #{results[:key]} and date #{results[:date]}"
