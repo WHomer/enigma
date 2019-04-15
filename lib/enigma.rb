b3cb722805181a09affd94c98887ef416c457148
William Homer <billyhomeriii@gmail.com>
2019-04-14 19:12:55 -0600

    Add the logic for the a,b,c,d keys

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,20 @@
 class Enigma
 
+  def encrypt(string, key, date)
+    a_key = key[1..2]
+    b_key = key[2..3]
+    c_key = key[3..4]
+    d_key = key[4..5]
+    string = ("a".."z").to_a << " "
+  end
 
+
+
+  # 1. Create a random 5 digit number
+  # 1a. take 1-2, 2-3, 3-4, 4-5 and 
+  # 1b. set them to a,b,c,d these are the keys
+  # 2. offset by date DDMMYY
+  # 2a. square root number
+  # 2b. take the last four numbers.
+  # 2c. take each digit and add them to key.
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,4 +10,9 @@ class EnigmaTest < MiniTest::Test
     assert_instance_of Enigma, @enigma_1
   end
 
+  def test_the_enigma_can_encrypt
+    result = {encryption: "keder ohulw", key: "02715", date: "040895"}
+    assert_equal result, @enigma_1.encrypt("hello world", "02715", "040895")
+  end
+
 end
